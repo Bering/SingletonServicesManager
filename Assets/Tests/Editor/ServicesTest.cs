0d05d49b4c1e8d07217fb2b6d86b6d9706a98299
Bering <bering@ringlogic.com>
2016-03-07 07:02:39 -0500

    First version

---
+++ Assets/Tests/Editor/ServicesTest.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class ServicesTest {
+
+	class MockService {}
+	class MockClient {}
+
+
+	[Test]
+	public void itThrowsExceptionIfFindingStuffThatIsNotRegistered()
+	{
+		Assert.Throws(typeof(Services.ServiceNotFoundException), FindNotRegisteredStuff);
+	}
+	protected void FindNotRegisteredStuff()
+	{
+		Services.Clear ();
+		Services.Find<MockService> ();
+	}
+
+
+	[Test]
+	public void itThrowsExceptionIfRegisteringStuffThatIsAlreadyRegistered()
+	{
+		Assert.Throws(typeof(Services.ServiceAlreadyRegisteredException), RegisterAlreadyRegisteredStuff);
+	}
+	protected void RegisterAlreadyRegisteredStuff()
+	{
+		Services.Clear ();
+		Services.Register<MockService> (new MockService());
+		Services.Register<MockService> (new MockService());
+	}
+
+
+	[Test]
+	public void itRegistersAServiceAndFindsItProperly()
+	{
+		Services.Clear ();
+
+		MockService A = new MockService();
+		MockService B = null;
+
+		Services.Register<MockService>(A);
+		B = Services.Find<MockService> ();
+			
+		Assert.AreSame (A, B);
+	}
+
+
+}
